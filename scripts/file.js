a6e21700d8cad0ad7239b3a5553688a045155201
Dillon Bussert <dillon.bussert@vidterra.com>
2022-01-17 12:59:11 -0500

    Update file reading script

--- scripts/file.js
+++ scripts/file.js
@@ -1,40 +1,10 @@
 const fs = require('fs')
 const { st0601, st0903, st0104, st0806, klv } = require('../index.js')
 
-const start = new Date().getTime() / 1000
 const standards = [st0601, st0903, st0806, st0104]
 const packets = {}
 for(const standard of standards) {
 	packets[standard.name] = []
-}
-
-async function processFile(file) {
-	let data = new Buffer.from('')
-	const options = { debug: process.argv[3] === 'debug' }
-
-	for await (const chunk of file) {
-		data = Buffer.concat([data, chunk])
-	}
-
-	for (let i = 0; i < data.length; i++) {
-		const buffer = data.subarray(i, data.length)
-
-		if (klv.startsWithKey(buffer, st0601.key)) {
-			const {index, values}  = klv.parseStandard(st0601, buffer, options)
-			values && packets.push(values)
-			i += index - 1
-		} else if (klv.startsWithKey(buffer, st0903.key)) {
-			const {index, values}  = klv.parseStandard(st0903, buffer, options)
-			values && packets.push(values)
-			i += index - 1
-		} else if (klv.startsWithKey(buffer, st0104.key)) {
-			const {index, values}  = klv.parseStandard(st0104, buffer, options)
-			values && packets.push(values)
-			i += index - 1
-		}
-	}
-
-	console.log(JSON.stringify(packets))
 }
 
 if (process.argv.length < 3) {
@@ -42,5 +12,19 @@ if (process.argv.length < 3) {
 	process.exit()
 }
 
-const file = fs.createReadStream(process.argv[2])
-processFile(file)
+fs.readFile(process.argv[2], (err, file) => {
+	const start = new Date().getTime() / 1000
+	const result = klv.decode(file, standards, null, { debug: process.argv[3] === 'debug', verbose: true })
+	for (const standard of standards) {
+		for(const packet of result[standard.name]) {
+			packets[standard.name].push(packet)
+		}
+	}
+
+	console.log(packets['st0601'][0])
+	for (const standard of standards) {
+		const name = standard.name
+		console.log(`${name}: ${packets[name]?.length ?? 0}`)
+	}
+	console.log(`Processing time ${(new Date().getTime() / 1000 - start).toFixed(2)}s`)
+})
